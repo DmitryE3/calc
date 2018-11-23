7b0f4bf24fd26b52db8d9069daee90641424a0c3
Dmitry P <toxpack@yandex.ru>
2018-11-23 14:01:28 +0300

    Add Fibona44i

---
+++ posledovatelnost_Fibona4i.py
@@ -0,0 +1,27 @@
+#Последовательность Фибоначчи
+#Введите число, и программа сгенерирует последовательность Фибоначчи до этого
+#числа или до N-го числа
+
+
+def fibonachi(list_fib,n):
+    if n==1 or n==2:
+        return 1
+    else:
+        while True:
+            x=list_fib[len(list_fib)-1]+list_fib[len(list_fib)-2]
+            if x>n:
+                break
+            else:
+                list_fib.append(x)
+                fibonachi(list_fib,n)
+        return list_fib
+
+def main():
+    list_fib=[1,1]
+    n=int(input('Введите число: '))
+    print(fibonachi(list_fib,n))
+    ext=input('Нажмите ENTR для выхода')
+
+
+if __name__=='__main__':
+    main()
