920a6d7aa5384bdade91d59c353b8afcc4d0657a
DmitryE3 <toxpack@yandex.ru>
2019-01-30 11:20:10 +0300

    Создан тест для Пиг_лата, первая проба unittest перенос в отдельную папку

---
+++ pig_lat/test_pig_lat.py
@@ -0,0 +1,10 @@
+import unittest
+import pig_lat
+
+class TestPigLat(unittest.TestCase):
+
+    def test_pig_lat(self):
+        self.assertEqual(pig_lat.make_pig_lat("banana"),"ananabay")
+
+if __name__ =="__main__":
+    unittest.main()
