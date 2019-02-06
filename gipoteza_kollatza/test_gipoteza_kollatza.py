7e15785db7ff1d8551ae33b937f078266e547874
DmitryE3 <toxpack@yandex.ru>
2019-02-06 11:46:57 +0300

    Все учебные задачки перекмнуты в отдельную папку

--- gipoteza_kollatza/test_gipoteza_kollatza.py
+++
@@ -1,9 +0,0 @@
-import unittest
-import Gipoteza_kollatza
-
-class TestKollatza(unittest.TestCase):
-    def test_gipoteza_kollatza(self):
-        self.assertEqual(Gipoteza_kollatza.gipoteza_kollatza(12),9)
-
-if __name__=='__main__':
-    unittest.main()
