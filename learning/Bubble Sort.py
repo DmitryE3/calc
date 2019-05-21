e8efe67468cdd57f4f9d1f39a457d0bc0cd6d5c0
DmitryE3 <toxpack@yandex.ru>
2019-05-21 12:51:47 +0300

    продолжаем учить threading

---
+++ learning/Bubble Sort.py
@@ -0,0 +1,14 @@
+oldlist=[10,75,43,15,25,-4,27]
+
+def bubble_sort(lst):
+    last_item=len(lst)-1
+    for j in range(last_item):
+        for i in range(last_item-j):
+            print(lst)
+            if lst[i]>lst[i+1]:
+                lst[i],lst[i+1]=lst[i+1],lst[i]
+    return lst
+
+new_list=bubble_sort(oldlist)
+print(new_list)
+print(oldlist)
