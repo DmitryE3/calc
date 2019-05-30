c84055d5f6f4d98942ea5fa580ffb6bf00414296
DmitryE3 <toxpack@yandex.ru>
2019-05-30 10:38:10 +0300

    just practice

---
+++ learning/intercalary_or_not_year.py
@@ -0,0 +1,10 @@
+year = None
+while not isinstance(year,int):
+    try:
+        year = int(input('Введите год: '))
+    except:
+        year = None
+if year%4!=0 or (year%100==0 and year%400!=0):
+    print('not intercalary year')
+else:
+    print('intercalary year')
---
+++ learning/sum_diag.py
@@ -0,0 +1,13 @@
+#В примере кода разработайте реализацию функции sumUpDiagonals() на JavaScript таким образом, чтобы она возвращала
+#суммы основной и вторичной диагоналей квадратной матрицы. (Переделано для работы на питон)
+
+def sumUpDiagonals(size_matrix,matrixExample):
+    summ_diag=0
+    sum_second_diag=0
+    for i in range(size_matrix):
+        summ_diag+=matrixExample[i][i]
+        sum_second_diag+=matrixExample[i][size_matrix-1-i]
+    return summ_diag, sum_second_diag
+
+matrixExample = [[ 1, 2, 3, 4 ],[ 4, 5, 6, 5 ],[ 7, 8, 9, 7 ],[ 7, 8, 9, 7 ]]
+print(sumUpDiagonals(4,matrixExample))
