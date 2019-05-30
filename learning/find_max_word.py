56c63c37ca8126bed5cebbd3b75ca479d5883d74
DmitryE3 <toxpack@yandex.ru>
2019-05-30 10:50:08 +0300

    just practice

---
+++ learning/find_max_word.py
@@ -0,0 +1,8 @@
+phrase=input('Введите предложение')
+print('Вы ввели предложение: ', phrase)
+words=phrase.split(' ')
+max_word = ''
+for word in words:
+    if len(word)>len(max_word):
+        max_word=word
+print(max_word)
