55a1b6788e8e368b5b450092ebd01e15eb0db8b7
DmitryE3 <toxpack@yandex.ru>
2019-10-02 11:18:28 +0300

    Задачи из курса ( в процессе )

---
+++ learning/download_image.py
@@ -0,0 +1,38 @@
+"""
+1. Осуществить ввод адреса Интернет-страницы.
+2. Осуществить поиск всех изображений на странице.
+3. Удалить дубликаты.
+4. Удалить адреса без http или https.
+5. Вывести адреса изображений с нумерацией, нумерацию изображений начинать с 1.
+6. Потоками осуществить загрузку изображений и информацию о загрузке вывести на экран отдельными строками
+7. В случае невозможности загрузки выводить 'error'.
+
+Пример входных данных:
+https://ya.ru/
+
+Пример выходных данных:
+1 izobrazhenie https://mc.yandex.ru/watch/723233
+Potok 1 zakonchil zagruzku https://mc.yandex.ru/watch/723233
+"""
+import threading
+import re
+from urllib.request import urlopen
+
+# Тут будет функция для скачивания
+def downloader():
+    pass
+
+
+#url = str(input())
+url = 'https://hh.ru/'
+f_html = urlopen(url)
+f_str = f_html.read()
+f_str = f_str.decode('utf-8')
+#f_bs = BS(f_str, 'lxml')
+#print(f_bs.find_all('img'))
+find_img = set(re.findall(r'src="(.+?)"', f_str))
+images = []
+for i in find_img:
+    if re.match('http', i):
+        images.append(i)
+print(images)
--- learning/parsing.py
+++ learning/parsing.py
@@ -1,7 +1,9 @@
 import re
+import ssl
+
+from bs4 import BeautifulSoup as BS
 from urllib.request import urlopen
 from urllib.parse import urlparse
-import ssl
 
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
@@ -9,16 +11,20 @@ ctx.verify_mode = ssl.CERT_NONE
 
 #url_x = str(input())
 #url_x = 'http://en.ifmo.ru/en/contacts/Contacts.htm'
-url_x = 'http://en.ifmo.ru/en/contacts/Contacts.htm'
+url_x = 'https://docs.djangoproject.com/en/1.11/'
 f_pars = tuple(urlparse(url_x))
 print(f_pars)
 f_html = urlopen(url_x, context = ctx)
 #f_html = open('mysite.html', 'r')
 f_str = f_html.read()
 f_str = f_str.decode('utf-8')
-find_h1 = re.findall(r'<h1>(.+?)</h1>', f_str)
+#f_bs = BS(f_str)
+#print(f_bs.h1)
+
+find_h1 = re.findall(r'<h1.+?>(.*?)</h1>', f_str)
 str_new = ''
 
+print(find_h1)
 for i in find_h1:
     str_new += i
-print(re.sub(r'<.*?>', '', str_new))
+print(re.sub(r'<.*?>', ' ', str_new))
