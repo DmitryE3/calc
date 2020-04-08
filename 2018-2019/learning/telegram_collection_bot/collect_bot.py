f1ae61b76c6a84e65667281873a1fd5e8e271d5f
DmitryE3 <dise3erli@gmail.com>
2020-04-08 14:00:26 +0300

    Работа с ботом

--- 2018-2019/learning/telegram_collection_bot/collect_bot.py
+++ 2018-2019/learning/telegram_collection_bot/collect_bot.py
@@ -1,9 +1,13 @@
 import telebot
 import os
 import re
+# from telebot import apihelper
+# apihelper.proxy = {'https': 'socks5://telegram.vpn.net:55555'}
 
-bot = telebot.TeleBot('1054467570:AAHTOT3ZxTo01yqh6JTZp4vc4Zxta5xmvzo')
-
+bot = telebot.TeleBot('')
+# тестовый словарь
+dict_collection = {'jack pott' : ['https://sun6-13.userapi.com/XhaZtgH3k_UN1HOCBFaHJU24UWVZ1s5ZT_L_MA/wosCN6IQ8T4.jpg',
+                                  'https://sun6-19.userapi.com/poIOzY9ezy_kfQ8bPujte5-fyyssBLhTZKZmxw/PhwnQAeChh4.jpg']}
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
@@ -14,19 +18,20 @@ def start_message(message):
 def send_text(message):
     bot.send_message(message.chat.id, 'Привет, ты ищешь {},'
                                       ' сейчас я покажу все, что есть в коллекции'.format(message.text))
-    directory = r'/home/oi/Python_repos/calc/learning/telegram_collection_bot/collection/'
-    name = os.listdir(directory)
     pics = []
-    pattern = re.compile(message.text.lower() + '\w+\.\w+')
-    for i in name:
-        x = pattern.search(i)
-        if x:
-            pics.append(x.group())
+    names = dict_collection.keys()
+    find_this = message.text.lower()
+    for name in names:
+        if find_this in name.lower():
+            pics.append(name)
     if len(pics) == 0:
         bot.send_message(message.chat.id, 'По вашему запросу ничего не найдено')
     else:
         for pic in pics:
-            bot.send_photo(message.chat.id, photo=open(directory + pic, 'rb'))
+            # bot.send_photo(message.chat.id, photo=open(directory + pic, 'rb'))
+            bot.send_message(message.chat.id, ' Найдено ' + str(len(pics)))
+            for i in dict_collection[pic]:
+                bot.send_photo(message.chat.id, photo=i)
 
 
 bot.polling()
