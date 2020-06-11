cf98b4264ea1ed986b3f6bf99fe21a16854216d0
DmitryE3 <dise3erli@gmail.com>
2020-06-11 13:48:20 +0300

    Фласк, базы данных

--- 2020/microblog/app/__init__.py
+++ 2020/microblog/app/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from config import Config    # импортируем объекты класса Config из модуля config
 
 app = Flask(__name__)
 app.config.from_object(Config)  # указываем, что для приложения app переменные конфига мы берем в Config
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-from app import routes
+from app import routes, models
--- 2020/microblog/config.py
+++ 2020/microblog/config.py
@@ -1,4 +1,10 @@
 import os
 
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 class Config():
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'something_very_secrets'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///'+os.path.join(basedir, 'app.db')
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False  #отмена сигнализации об изменениях в БД
