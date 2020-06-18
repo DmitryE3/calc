02706c6e83ce8d2d876e953df7bd213f4295af76
DmitryE3 <toxpack@yandex.ru>
2020-06-18 16:20:54 +0300

    Работа продолжается

--- 2020/microblog/app/models.py
+++ 2020/microblog/app/models.py
@@ -1,11 +1,11 @@
 from app import db
-from flask_login import UserMixin #+ к объектам юзер 4 элемента (is_authenticated, is_active, is_anonymus, get_id()
+from flask_login import UserMixin  # + к объектам юзер 4 элемента (is_authenticated, is_active, is_anonymus, get_id()
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from app import login
 
 
-class User(UserMixin,db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -30,6 +30,7 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
 
 @login.user_loader
 def load_user(id):
--- 2020/microblog/app/routes.py
+++ 2020/microblog/app/routes.py
@@ -1,18 +1,20 @@
 from app import app
-from flask import render_template, flash, url_for, redirect
-from flask_login import current_user, login_user, logout_user
+from flask import render_template, flash, url_for, redirect, request
+from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
 from app.forms.forms import LoginForm
+from werkzeug.urls import url_parse
 
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     user = {'username': 'Diman'}
     posts = [{'author': {'username': 'Vladimir Lenin'}, 'body': 'Learn, Laern, Laern'},
              {'author': {'username': 'Aleksandr Pushkin'}, 'body': 'Evgeniy Inegin rulez'},
              {'author': {'username': 'Karl Marx'}, 'body': 'Working class not for us'}]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home', posts=posts)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -21,12 +23,15 @@ def login():
         return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(username=form.usernamde.data).first()
-        if user is None or not User.check_password(form.password.data):
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(password=form.password.data):
             flash('Invalid username or password. Please try again')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data) # Обработка логина пользователя
-        return redirect(url_for('index'))
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='SignIn', form=form)
 
 
