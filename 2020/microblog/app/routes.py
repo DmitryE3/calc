82b3c4147df4940b4c8cfe00a2d3401f8e8a5f98
DmitryE3 <toxpack@yandex.ru>
2020-06-26 15:01:55 +0300

    Add Edit Profile function

--- 2020/microblog/app/routes.py
+++ 2020/microblog/app/routes.py
@@ -2,10 +2,10 @@ from app import app
 from flask import render_template, flash, url_for, redirect, request
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
-from app.forms.forms import LoginForm
+from app.forms.forms import LoginForm, RegistrationForm, EditProfileForm
 from werkzeug.urls import url_parse
 from app import db
-from app.forms.forms import RegistrationForm
+from datetime import datetime
 
 
 @app.route('/')
@@ -67,3 +67,26 @@ def user(username):
         {'author': user, 'body': 'Test post 666'}
     ]
     return render_template('user.html', user=user, posts=posts)
+
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
+
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved!')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile', form=form)
